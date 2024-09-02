144d9df2c9b5be7a884d084b62f1f244c94f3ad1
tirth-bhalodiya <tirthbhalodia3029@gmail.com>
2024-09-03 01:39:32 +0530

    main Initial commit for repo scanning and file creation

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+)
+
+func main() {
+	var folder string
+	var email string
+	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repositories")
+	flag.StringVar(&email, "email", "youremail@gmail.com", "the email to scan")
+	flag.Parse()
+
+	if folder != "" {
+		scan(folder)
+		return
+	}
+
+	stats(email)
+
+}
